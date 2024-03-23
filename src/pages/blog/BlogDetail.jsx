4560f9495dbace97afdf10d13ba4d3dbe76870bc
Bashirr <beshirr096@gmail.com>
2024-03-23 19:07:35 +0400

    blog grid card created and completed

--- src/pages/blog/BlogDetail.jsx
+++ src/pages/blog/BlogDetail.jsx
@@ -1,9 +1,16 @@
+import {useContext} from "react";
+import {PostsContext} from "../../context/Posts/PostsContext";
+import {useParams} from "react-router-dom";
+
 const BlogDetail = () => {
+    const {posts} = useContext(PostsContext);
+    const {id} = useParams();
+    const post = posts.find(post => post.id === parseInt(id));
     return (
         <>
             <section>
                 <div className="container">
-                    blog detail
+                    {post.title}
                 </div>
             </section>
         </>
