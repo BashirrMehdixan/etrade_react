da44ad653b7987fecc51136241bb696a25a5e14d
Bashirr <beshirr096@gmail.com>
2024-03-25 13:48:45 +0400

    blog list completed

--- src/components/BlogCards.jsx
+++ src/components/BlogCards.jsx
@@ -2,27 +2,42 @@ import {useContext} from "react";
 import {Link} from "react-router-dom";
 import {UsersContext} from "context/Users/UsersContext";
 
-export const BlogListCard = ({post}) => {
-    return (
-        <>
-
-        </>
-    )
-}
-
 export const BlogGridCard = ({post}) => {
     const {users} = useContext(UsersContext);
     const authUser = users.find(user => user.id === post.userId);
-    console.log(authUser)
     return (
         authUser &&
         <>
+            <div className="blog-list-item">
+                <h3 className="blog-title">
+                    <Link to={post.id.toString()}>
+                        “An oral history of the AIM away message (by the people who were there)”
+                    </Link>
+                </h3>
+                <div className="post-auth">
+                    <div className="auth-img">
+                        <img src={authUser.image} alt={authUser.name}/>
+                    </div>
+                    <div className="post-info">
+                        <h5 className="auth-name">
+                            {authUser.firstName} {authUser.lastName}
+                        </h5>
+                        <div className="post-date">
+                            Mar 25, 2022
+                        </div>
+                    </div>
+                </div>
+            </div>
             <div className="blog-grid-item">
                 <div className="blog-img">
-                    <img src={"./assets/images/blog/blog-img.png"} alt=""/>
+                    <Link to={post.id.toString()}>
+                        <img src={"./assets/images/blog/blog-img.png"} alt=""/>
+                    </Link>
                 </div>
                 <h3 className="blog-title">
-                    {post.title}
+                    <Link to={post.id.toString()}>
+                        {post.title}
+                    </Link>
                 </h3>
                 <div className="post-auth">
                     <div className="auth-img">
